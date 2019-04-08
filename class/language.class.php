4ad4b487dbf14698f549fc86e1e51efe81b802eb
Juhani Järviö <juhani.jarvio@gmail.com>
2019-04-08 11:17:32 +0300

    Update language.class.php

--- class/language.class.php
+++ class/language.class.php
@@ -6,16 +6,18 @@ class Language extends stdClass {
 	 * @var string $lang Three character language code ISO 639-2/T
 	 */
 	public $lang;
+	public $page;
 
-	function __construct( DByhteys $db, string $lang, string $page ) {
+	function __construct( DBConnection $db, string $lang = 'eng', string $page = CURRENT_PAGE ) {
 
 		$this->lang = $lang;
+		$this->page = $page;
 
 		$sql = "select txt_type, txt 
 				from lang
 				where lang = ? 
 				  and (txt_page = ? or txt_page = '_common')";
-		$rows = $db->query( $sql, [ $lang, $page ], DByhteys::FETCH_ALL );
+		$rows = $db->query( $sql, [ $lang, $page ], FETCH_ALL );
 
 		foreach ( $rows as $row ) {
 			$this->{$row->txt_type} = $row->txt;

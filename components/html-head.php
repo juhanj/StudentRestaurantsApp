38bdc04cb91c18e244c10a7f2839c7ef7688aaee
Juhani Järviö <juhani.jarvio@gmail.com>
2019-04-08 11:18:40 +0300

    New components for HTML
    head, header, footer.
    
    CSS in separate update.

---
+++ components/html-head.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1); ?>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title><?= $lang->HTML_TITLE ?></title>
+
+	<link rel="icon" href="<?= WEB_PATH ?>/img/favicon-anim.gif" type="image/gif">
+
+	<!-- Material icons CSS -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
+	<!-- Modern-Normalize CSS -->
+	<link rel="stylesheet" href="<?= WEB_PATH ?>/css/modern-normalize.css">
+
+	<!-- Main CSS file -->
+	<link rel="stylesheet" href="<?= WEB_PATH ?>/css/main.css">
+	<!-- Header-footer CSS file -->
+	<link rel="stylesheet" href="<?= WEB_PATH ?>/css/header-footer.css">
+	<!-- Page specific CSS file -->
+	<link rel="stylesheet" href="<?= WEB_PATH ?>/css/<?= CURRENT_PAGE ?>.css">
+
+	<!-- Main javascript file -->
+	<script defer src="<?= WEB_PATH ?>/js/main.js"></script>
+</head>
---
+++ components/html-header.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1); ?>
+
+<header class="page margins-off" id="page-header">
+	<div class="left">
+		<span class="joke" title="Hello World">👋 🌍</span>
+	</div>
+	<div class="center">
+		<h1 class="page-title"><?= $lang->HEADER_NAME ?></h1>
+	</div>
+	<div class="right">
+		<a href="<?= WEB_PATH ?>/settings.php">Settings</a>
+	</div>
+</header>
